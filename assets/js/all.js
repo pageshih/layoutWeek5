d1107a7504250a39899b48ab7cd408fc736a986a
pageshih <a0981863141@gmail.com>
2021-08-17 15:24:19 +0800

    Update 2021-08-17T07:24:15.783Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -11,4 +11,16 @@ $("#navItem").click(function (e) {
   $(".current-page").removeClass("current-page");
   $(e.target).parent().addClass("current-page");
 });
+$("#reply").click(function () {
+  $("#mentor,#endBtn").removeClass("d-flex");
+  $("#mentor,#endBtn").addClass("d-none");
+  $("#editArea").addClass("show");
+  $("#cardPb0").addClass("pb-0");
+});
+$("#editCancel").click(function () {
+  $("#editArea").removeClass("show");
+  $("#mentor,#endBtn").removeClass("d-none");
+  $("#mentor,#endBtn").addClass("d-flex");
+  $("#cardPb0").removeClass("pb-0");
+});
 //# sourceMappingURL=all.js.map
